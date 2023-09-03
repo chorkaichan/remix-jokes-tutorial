7ce39aff6c780c6337b2713b8cc5b9c1529ce749
Kaito Kikuchi <sin180degree@gmail.com>
2023-09-03 21:24:46 +0900

    added hello world

--- app/root.tsx
+++ app/root.tsx
@@ -1,31 +1,18 @@
-import type { LinksFunction } from "@remix-run/cloudflare";
-import { cssBundleHref } from "@remix-run/css-bundle";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import { LiveReload } from "@remix-run/react";
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <title>Remix: So great, it's funny!</title>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        Hello world
         <LiveReload />
       </body>
     </html>
